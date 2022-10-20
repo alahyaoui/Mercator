dfa331b7c921475df8202fded931e0c6518d95f4
Ayoub Lahyaoui <ayoub.lahyaoui@hotmail.com>
2022-10-21 01:09:11 +0200

    Add prettier and configure eslint

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import { useState } from 'react';
-import { StyleSheet, View, Button, TextInput } from 'react-native';
-import { WebView } from 'react-native-webview';
-import MapTemplate from './MapTemplate';
-
+import React from "react";
+import { useState } from "react";
+import { StyleSheet, View, Button, TextInput } from "react-native";
+import { WebView } from "react-native-webview";
+import MapTemplate from "./MapTemplate";
 
 export default function App() {
   let webRef = undefined;
-  let [mapCenter, setMapCenter] = useState('-121.913, 37.361');
+  let [mapCenter, setMapCenter] = useState("-121.913, 37.361");
   const run = `
       document.body.style.backgroundColor = 'blue';
       true;
@@ -16,26 +15,27 @@ export default function App() {
   const onButtonClick = () => {
     const [lng, lat] = mapCenter.split(",");
     webRef.injectJavaScript(`map.setCenter([${parseFloat(lng)}, ${parseFloat(lat)}])`);
-  }
+  };
 
   const handleMapEvent = (event) => {
-    setMapCenter(event.nativeEvent.data)
-  }
+    setMapCenter(event.nativeEvent.data);
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.buttons}>
-        <TextInput 
-        style={styles.textInput}
-        onChangeText={setMapCenter}
-        value={mapCenter}></TextInput>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={setMapCenter}
+          value={mapCenter}
+        ></TextInput>
         <Button title="Set Center" onPress={onButtonClick}></Button>
       </View>
       <WebView
         ref={(r) => (webRef = r)}
         onMessage={handleMapEvent}
         style={styles.map}
-        originWhitelist={['*']}
+        originWhitelist={["*"]}
         source={{ html: MapTemplate }}
       />
     </View>
@@ -44,17 +44,17 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'column',
-    flex: 1
+    flexDirection: "column",
+    flex: 1,
   },
   buttons: {
-    flexDirection: 'row',
-    height: '15%',
-    backgroundColor: '#fff',
-    color: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 12
+    flexDirection: "row",
+    height: "15%",
+    backgroundColor: "#fff",
+    color: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 12,
   },
   textInput: {
     height: 40,
@@ -64,9 +64,9 @@ const styles = StyleSheet.create({
     borderWidth: 1,
   },
   map: {
-    width: '100%',
-    height: '85%',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "100%",
+    height: "85%",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
